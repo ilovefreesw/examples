587c36121241e6d4aa9f95e6195d51f2f79bfa9a
Laxman <77317006+ilovefreesw@users.noreply.github.com>
2023-06-24 16:17:51 +0530

    Update main.py

--- src/01 PyQt QLabel/main.py
+++ src/01 PyQt QLabel/main.py
@@ -1,5 +1,9 @@
 from PyQt6.QtWidgets import *
+import time
+
+time.sleep(2)
+
 app = QApplication([])
-label = QLabel('Hello World!')
+label = QLabel('Hello World I am back!')
 label.show()
 app.exec()
